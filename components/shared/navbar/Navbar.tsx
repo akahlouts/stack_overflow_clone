73ac105ee106eaeef172f61e455b6c5082929360
AbdElRahman KahlOut <akahlouts@gmail.com>
2024-08-06 20:15:53 +0300

    add moblieNav component.

--- components/shared/navbar/Navbar.tsx
+++ components/shared/navbar/Navbar.tsx
@@ -6,6 +6,7 @@ import Image from "next/image";
 import { SignedIn, UserButton } from "@clerk/nextjs";
 
 import Theme from "./Theme";
+import MobileNav from "./MobileNav";
 
 const Navbar = () => {
   return (
@@ -38,7 +39,7 @@ const Navbar = () => {
             }}
           />
         </SignedIn>
-        {/* MobileNav */}
+        <MobileNav />
       </div>
     </nav>
   );
