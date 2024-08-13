955e8db285af5d45a7921f82750b2fd017ed4469
AbdElRahman KahlOut <akahlouts@gmail.com>
2024-08-14 00:27:32 +0300

    create Question action

--- lib/actions/question.action.ts
+++ lib/actions/question.action.ts
@@ -2,10 +2,39 @@
 
 import { connectToDatabase } from "../mongoose";
 
+import Question from "@/database/question.model";
+import Tag from "@/database/tag.model";
+
 export async function createQuestion(params: any) {
   // eslint-disable-next-line no-empty
   try {
     // connect to DB
     connectToDatabase();
+
+    const { title, content, tags, author, path } = params;
+
+    // Create the question
+    const question = await Question.create({
+      title,
+      content,
+      author,
+    });
+
+    const tagDocuments = [];
+
+    // Create the tags or get them if they already exist
+    for (const tag of tags) {
+      const existingTag = await Tag.findOneAndUpdate(
+        { name: { $regex: new RegExp(`^${tag}$`, "i") } },
+        { $setOnInsert: { name: tag }, $push: { question: question._id } },
+        { upsert: true, new: true }
+      );
+
+      tagDocuments.push(existingTag._id);
+    }
+
+    await Question.findByIdAndUpdate(question._id, {
+      $push: { tags: { $each: tagDocuments } },
+    });
   } catch (error) {}
 }
