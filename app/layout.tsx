51984ee3bb36991c9dfae29244a409e3c9230905
AbdElRahman KahlOut <akahlouts@gmail.com>
2024-08-15 18:51:23 +0300

    fix eslint errors

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
+/* eslint-disable camelcase */
 import React from "react";
 import { ClerkProvider } from "@clerk/nextjs";
 import { ThemeProvider } from "@/context/ThemeProvider";
-// eslint-disable-next-line camelcase
 import { Inter, Space_Grotesk } from "next/font/google";
 import type { Metadata } from "next";
 
