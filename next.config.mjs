c847494c791084cc5481839ba7e7da70b1b8c467
AbdElRahman KahlOut <akahlouts@gmail.com>
2024-08-16 06:49:54 +0300

    modify next config & middleware

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,11 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "randomuser.me",
+        hostname: "*",
+      },
+      {
+        protocol: "http",
+        hostname: "*",
       },
     ],
   },
