9253efe376817d27bd7531ab22d106d3acbbb72d
AbdElRahman KahlOut <akahlouts@gmail.com>
2024-08-24 19:26:15 +0300

    Show Top Questions

--- components/shared/RightSidebar.tsx
+++ components/shared/RightSidebar.tsx
@@ -3,19 +3,9 @@ import React from "react";
 import Link from "next/link";
 import Image from "next/image";
 
-import RenderTag from "./RenderTag";
+import { getHotQuestions } from "@/lib/actions/question.action";
 
-const hotQuestions = [
-  {
-    _id: "1",
-    title:
-      "Best practices for data fetching in a Next.js application with Server-Side Rendering (SSR)?",
-  },
-  { _id: "2", title: "Is it only me or the font is bolder than necessary?" },
-  { _id: "3", title: "Redux Toolkit Not Updating State as Expected" },
-  { _id: "4", title: "Can I get the course for free?" },
-  { _id: "5", title: "Async/Await Function Not Handling Errors Properly" },
-];
+import RenderTag from "./RenderTag";
 
 const popularTags = [
   { _id: "1", name: "javascript", totalQuestions: 5 },
@@ -25,7 +15,9 @@ const popularTags = [
   { _id: "5", name: "redux", totalQuestions: 10 },
 ];
 
-const RightSidebar = () => {
+const RightSidebar = async () => {
+  const hotQuestions = await getHotQuestions();
+
   return (
     <section className="background-light900_dark200 light-border custom-scrollbar sticky right-0 top-0 flex h-screen w-[350px] flex-col overflow-y-auto border-l p-6 pt-36 shadow-light-300 dark:shadow-none max-xl:hidden">
       <div className="">
@@ -33,7 +25,7 @@ const RightSidebar = () => {
         <div className="mt-7 flex w-full flex-col gap-[30px]">
           {hotQuestions.map((question) => (
             <Link
-              href={`/questions/${question._id}`}
+              href={`/question/${question._id}`}
               key={question._id}
               className="flex cursor-pointer items-center justify-between gap-7"
             >
