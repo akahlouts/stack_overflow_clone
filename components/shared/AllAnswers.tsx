abc581bb7f7f974de301d260c09c6e8c16f7a6ba
AbdElRahman KahlOut <akahlouts@gmail.com>
2024-08-27 01:29:03 +0300

    ..

--- components/shared/AllAnswers.tsx
+++ components/shared/AllAnswers.tsx
@@ -43,7 +43,7 @@ const AllAnswers = async ({
       <div>
         {result.answers.map((answer) => (
           <article key={answer._id} className="light-border border-b py-10">
-            <div className="mb-8 flex flex-col-reverse justify-between gap-5 sm:flex-row sm:items-center sm:gap-2">
+            <div className="mb-5 flex flex-col-reverse justify-between gap-5 sm:flex-row sm:items-center sm:gap-2">
               <Link
                 href={`/profile/${answer.author.clerkId}`}
                 className="flex flex-1 items-start gap-1 sm:items-center"
