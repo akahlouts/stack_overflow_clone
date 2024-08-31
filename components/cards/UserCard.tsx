3f074d72f7bb952078efaa66d8bec04c34e043bf
AbdElRahman KahlOut <akahlouts@gmail.com>
2024-08-31 10:12:40 +0300

    fix...

--- components/cards/UserCard.tsx
+++ components/cards/UserCard.tsx
@@ -22,9 +22,9 @@ const UserCard = async ({ user }: Props) => {
   return (
     <Link
       href={`/profile/${user.clerkId}`}
-      className="shadow-light100_darknone w-full max-xs:min-w-full xs:w-[260px]"
+      className="shadow-light100_darknone w-full max-xs:min-w-full xs:w-[280px]"
     >
-      <article className="background-light900_dark200 light-border flex w-full flex-col items-center justify-center rounded-2xl border p-8">
+      <article className="background-light900_dark200 light-border flex w-full flex-col items-center justify-center rounded-2xl border p-10">
         <Image
           src={user.picture}
           alt="user profile picture"
@@ -44,7 +44,7 @@ const UserCard = async ({ user }: Props) => {
 
         <div className="mt-5">
           {tags.length > 0 ? (
-            <div className="flex flex-wrap items-center justify-center gap-3">
+            <div className="flex items-center justify-center gap-3">
               {tags.map((tag: any) => (
                 <RenderTag key={tag._id} _id={tag._id} name={tag.name} />
               ))}
