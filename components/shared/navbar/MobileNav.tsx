be6189d921736746dcb8f47c7fb4c34945897b84
AbdElRahman KahlOut <akahlouts@gmail.com>
2024-08-31 11:29:44 +0300

    ...

--- components/shared/navbar/MobileNav.tsx
+++ components/shared/navbar/MobileNav.tsx
@@ -84,7 +84,7 @@ const MobileNav = () => {
           </SheetClose>
 
           <SignedOut>
-            <div className="flex flex-col gap-3 justify-end">
+            <div className="flex flex-col gap-3">
               <SheetClose asChild>
                 <Link href="/sign-in">
                   <Button className="small-medium btn-secondary min-h-[41px] w-full rounded-lg px-4 py-3 shadow-none focus-visible:ring-0 focus-visible:ring-offset-0">
